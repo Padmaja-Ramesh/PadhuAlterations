f3a8dd6d438ec9afb1a756d56f5df84551851ad0
padmaja-ramesh <padmaja-ramesh@users.noreply.github.com>
2025-10-14 12:38:45 -0400

    template for the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to Padhu Alternations</h1>
+      <p>
+        Online and On-Site Tailoring Custom Alterations for Men & Women Get your
+        clothing altered quickly and easily by our expert tailors &
+        seamstresses,with only a week turn around.
+      </p>
+    </>
+  );
+}
+
+export default App;
